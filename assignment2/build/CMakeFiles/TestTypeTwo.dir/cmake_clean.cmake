f304311a8bd3a9d3c2ce04195c79a7a408b1439d
Stella Zarei <zareistella@gmail.com>
2023-07-22 19:44:30 -0700

    sync changes from local machine

---
+++ assignment2/build/CMakeFiles/TestTypeTwo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestTypeTwo.dir/TestTypeTwo.cpp.o"
+  "TestTypeTwo"
+  "TestTypeTwo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestTypeTwo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
